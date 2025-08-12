235aa27b29692988fcea789252299db0f8cfe3d8
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-08-12 13:46:29 -0300

    Screen update

--- src/templates/merolounge/sections/hero-section/hero-section.tsx
+++ src/templates/merolounge/sections/hero-section/hero-section.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export function HeroSection() {
   return (
-    <div className="relative md:h-screen w-full bg-gray-900">
+    <div className="relative h-screen w-full bg-gray-900">
       <video
         src="/mero-lounge-video.mp4"
         autoPlay
@@ -14,7 +14,7 @@ export function HeroSection() {
       ></video>
 
       <div>
-        <div className="absolute z-40 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+        <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
           <h1 className="text-xl sm:text-5xl md:text-7xl font-semibold mb-4 uppercase tracking-[3px]">
             Mero Lounge
           </h1>
--- src/templates/meromar/sections/hero-section/hero-section.tsx
+++ src/templates/meromar/sections/hero-section/hero-section.tsx
@@ -2,7 +2,7 @@
 
 export function HeroSection() {
   return (
-    <div className="relative md:h-screen w-full bg-gray-900">
+    <div className="relative h-screen w-full bg-gray-900">
       <video
         src="/mero-mar-hote-video.mp4"
         autoPlay
@@ -12,7 +12,7 @@ export function HeroSection() {
       ></video>
 
       <div>
-        <div className="absolute z-40 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+        <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
           <h1 className="text-xl sm:text-5xl md:text-7xl font-semibold mb-4 uppercase tracking-[3px]">
             Mero Mar Hotel
           </h1>
