dee7dbfda342352ae5a4c388fa1f0de39feb466f
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-09-01 17:43:02 -0300

    Tudo certo

--- src/templates/meroboutique/sections/gallery-section/gallery-section.tsx
+++ src/templates/meroboutique/sections/gallery-section/gallery-section.tsx
@@ -3,7 +3,7 @@ import { CarouselGallery } from "./carousel-gallery";
 
 export function GallerySection() {
   return (
-    <section className="relative  px-4 py-4 md:py-18 flex flex-col items-center justify-center bg-gray-300">
+    <section className="relative px-4 py-8 md:py-16 flex flex-col items-center justify-center bg-gray-300">
       <div className="relative w-full max-w-6xl text-center mx-auto space-y-18 my-16">
         <h2 className="text-xl sm:text-3xl font-semibold tracking-[0.6rem] sm:tracking-[0.8rem] uppercase">
           Galeria de Fotos
