4c3d7c72b234a675cd60a568cf77c0424b3255a3
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-09-03 16:25:58 -0300

    add seo metadata in gastronomia & experience page

--- src/app/(meroboutique)/meroboutique/experiencias/page.tsx
+++ src/app/(meroboutique)/meroboutique/experiencias/page.tsx
@@ -1,4 +1,29 @@
 import { ExperiencePage } from "@/templates/meroboutique-experience";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mero Boutique | Experiências",
+  description:
+    "No Mero Hotel Boutique, em Barra Grande-PI, o sol se ergue em memórias. Nosso oásis mediterrâneo sussurra conforto com piscinas ou jacuzzis privativas, toques de bem-estar e o abraço do Mero Mar Lounge. Desperte para um delicioso café da manhã!",
+  openGraph: {
+    title: "Mero Boutique | Experiências",
+    description:
+      "No Mero Hotel Boutique, em Barra Grande-PI, o sol se ergue em memórias. Nosso oásis mediterrâneo sussurra conforto com piscinas ou jacuzzis privativas, toques de bem-estar e o abraço do Mero Mar Lounge. Desperte para um delicioso café da manhã!",
+    url: "https://www.grupomero.com.br/meroboutique/acomodacoes",
+    siteName: "Mero Boutique | Experiências",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://www.grupomero.com.br/meroboutique/experiences/bg-memoravel-2.jpg",
+        width: 800,
+        height: 600,
+        alt: "Mero Boutique | Experiências",
+      },
+    ],
+  },
+};
+
 
 export default function Experience() {
   return <ExperiencePage />;
--- src/app/(meroboutique)/meroboutique/gastronomia/page.tsx
+++ src/app/(meroboutique)/meroboutique/gastronomia/page.tsx
@@ -1,4 +1,28 @@
 import { GastronomiaPage } from "@/templates/meroboutique-gastronomia";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mero Boutique | Gastronomia",
+  description:
+    "No Mero Hotel Boutique, em Barra Grande-PI, o sol se ergue em memórias. Nosso oásis mediterrâneo sussurra conforto com piscinas ou jacuzzis privativas, toques de bem-estar e o abraço do Mero Mar Lounge. Desperte para um delicioso café da manhã!",
+  openGraph: {
+    title: "Mero Boutique | Gastronomia",
+    description:
+      "No Mero Hotel Boutique, em Barra Grande-PI, o sol se ergue em memórias. Nosso oásis mediterrâneo sussurra conforto com piscinas ou jacuzzis privativas, toques de bem-estar e o abraço do Mero Mar Lounge. Desperte para um delicioso café da manhã!",
+    url: "https://www.grupomero.com.br/meroboutique/acomodacoes",
+    siteName: "Mero Boutique | Gastronomia",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://www.grupomero.com.br/meroboutique/experiences/folia-gourmet-1.jpg",
+        width: 800,
+        height: 600,
+        alt: "Mero Boutique | Gastronomia",
+      },
+    ],
+  },
+};
 
 export default function Gastronomia() {
   return <GastronomiaPage />;
